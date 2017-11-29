2962f3b2369f69e70949f0f972a502358467cfc4
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-11-29 13:18:41 -0800

    Added and Updated Sign In Redirects

--- PropoPro/app/controllers/pages_controller.rb
+++ PropoPro/app/controllers/pages_controller.rb
@@ -6,10 +6,18 @@ class PagesController < ApplicationController
   end
 
   def creative
+    binding.pry
+    if admin_signed_in?
+      sign_out current_user
+      redirect_to crtv_path
+    end
   end
 
   def companydash
-    binding.pry
+    if user_signed_in?
+      sign_out current_admin
+      redirect_to cmpny_path
+    end
   end
 
   def creativedash
